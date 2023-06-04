83c823d27c0a71ccee35247c163e936d08689392
98-9 <hatamlehluma35@gmail.com>
2023-06-04 22:55:46 +0000

    adding assets to the file

--- app.js
+++ app.js
@@ -30,11 +30,12 @@ function newSalary (level) {
 }
 
 // constructor function
-function Employee(fullName, department, level) {
+function Employee(fullName, department, level, imageURL) {
   this.employeeId = generateId();
   this.fullName = fullName;
   this.department = department;
   this.level = level;
+  this.imageURL = imageURL
   this.salary = 0;
 }
 Employee.prototype.netSalary= function(){
@@ -43,15 +44,15 @@ Employee.prototype.netSalary= function(){
 
 
 
-let Ghazi = new Employee('Ghazi Samer', "Administration", "Senior");
-let Lana = new Employee('Lana Ali', "Finance", "Senior");
-let Tamara = new Employee('Tamara Ayoub', "Marketing", "Senior");
-let Safi = new Employee('Safi Walid', "Administration", "Mid-Senior");
-let Omar = new Employee('Omar Zaid', "Development", "Senior");
-let Rana = new Employee('Rana Saleh', "Development", "Junior");
-let Hadi = new Employee('Hadi Ahmad', "Finance", "Mid-Senior");
+let Ghazi = new Employee('Ghazi Samer', "Administration", "Senior", "./assets/Ghazi.png");
+let Lana = new Employee('Lana Ali', "Finance", "Senior", "./assets/Lana.png");
+let Tamara = new Employee('Tamara Ayoub', "Marketing", "Senior", "./assets/Tamara.png");
+let Safi = new Employee('Safi Walid', "Administration", "Mid-Senior", "./assets/Safi.png");
+let Omar = new Employee('Omar Zaid', "Development", "Senior", "./assets/Omar.png");
+let Rana = new Employee('Rana Saleh', "Development", "Junior", "./assets/Rana.png");
+let Hadi = new Employee('Hadi Ahmad', "Finance", "Mid-Senior", "./assets/Hadi.png");
 
-console.table(Ghazi);
+// console.table(Ghazi);
 Ghazi.netSalary()
 Lana.netSalary()
 Tamara.netSalary()
@@ -59,6 +60,28 @@ Safi.netSalary()
 Omar.netSalary()
 Rana.netSalary()
 Hadi.netSalary()
+
+//const obj_______________________________________________
+console.table(Ghazi);
+console.table(Lana);
+console.table(Tamara);
+console.table(Safi);
+console.table(Omar);
+console.table(Rana);
+console.table(Hadi);
+
+//properties method_________________________________________________
+const mainEl = document.getElementsByTagName("main");
+
+Employee.prototype.render = function () {
+    const divParentEl = document.createElement("div")
+    divParentEl.className = "card"
+    mainEl[0].appendChild(divParentEl)
+
+    const divEl = document.createElement("div")
+    divEl.className = "imageURL"
+    divParentEl.appendChild(divEl)}
+
 
 
 //Employee prototype
@@ -69,6 +92,8 @@ Employee.prototype.render = function () {
   document.write(`<p><label>department:</label><span class="department" id="department">${this.department}</span></p>`);
   document.write(`<p><label>Level:</label><span class="level" id="level">${this.level}</span></p>`);
   document.write(`<p><label>Salary:</label><span class="salary" id="salary">${this.salary}</span></p>`);
+  document.write(`<p><label>imageURL:</label><span class="imageURl" id="imageURL">${this.imageURL}</span></p>`);
+
 };
 
 // Create an array to hold the employee objects
@@ -78,4 +103,5 @@ let employees = [Ghazi, Lana, Tamara, Safi, Omar, Rana, Hadi];
 for (let i = 0; i < employees.length; i++) {
   employees[i].render();
   document.write("<hr>")
+
 }
